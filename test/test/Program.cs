36d18129bf92bd99fba2833f9d05e20b0653c190
w464144838 <w464144838@163.com>
2016-10-11 13:38:11 +0800

    测试修改

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -14,6 +14,7 @@ namespace test
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
